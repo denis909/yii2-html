6f250bb8fa65bd38e09111c336efd9a152953d57
denis909 <denis909@mail.ru>
2020-06-10 12:12:25 +0300

    n/a

--- Html.php
+++ Html.php
@@ -2,12 +2,12 @@
 
 namespace denis909\yii;
 
-use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 
-class Html extends yii\helpers\Html
+class Html extends \yii\helpers\Html
 {
 
-    public $submitButtonOptions = ['class' => 'btn btn-primary'];
+    static $submitButtonOptions = ['class' => 'btn btn-primary'];
 
     public static function submitButton($content = null, $options = [])
     {
@@ -16,9 +16,9 @@ class Html extends yii\helpers\Html
             $content = Yii::t('app', 'Submit');
         }
 
-        $options = ArrayHelper::merge($this->submitButtonOptions, $options);
+        $options = ArrayHelper::merge(static::$submitButtonOptions, $options);
 
-        return static::submitButton($content, $options);
+        return parent::submitButton($content, $options);
     }
 
 }
