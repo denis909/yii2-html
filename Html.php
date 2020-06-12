1fa5b664c2833b496442152956ebb8f3be029240
denis909 <denis909@mail.ru>
2020-06-12 10:49:14 +0300

    n/a

--- Html.php
+++ Html.php
@@ -8,6 +8,10 @@ class Html extends \yii\helpers\Html
 {
 
     static $submitButtonOptions = ['class' => 'btn btn-primary'];
+    
+    static $cancelButtonOptions = ['class' => 'btn btn-secondary'];
+    
+    static $saveButtonOptions = ['class' => 'btn btn-success'];
 
     public static function submitButton($content = null, $options = [])
     {
@@ -17,6 +21,30 @@ class Html extends \yii\helpers\Html
         }
 
         $options = ArrayHelper::merge(static::$submitButtonOptions, $options);
+
+        return parent::submitButton($content, $options);
+    }
+    
+    public static function cancelButton($content = null, $options = [])
+    {
+        if (!$content)
+        {
+            $content = Yii::t('app', 'Cancel');
+        }
+
+        $options = ArrayHelper::merge(static::$cancelButtonOptions, $options);
+
+        return parent::cancelButton($content, $options);
+    }
+    
+    public static function saveButton($content = null, $options = [])
+    {
+        if (!$content)
+        {
+            $content = Yii::t('app', 'Save');
+        }
+
+        $options = ArrayHelper::merge(static::$saveButtonOptions, $options);
 
         return parent::submitButton($content, $options);
     }
